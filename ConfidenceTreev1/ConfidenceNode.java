1a84634ccc8bb342bad4fecb2cdf6a0fa4e0a760
Craig Colquitt <colquitt.craig@gmail.com>
2018-05-22 09:45:18 -0600

    Trying again

---
+++ ConfidenceTreev1/ConfidenceNode.java
@@ -0,0 +1,24 @@
+
+
+public class ConfidenceNode {
+
+	//ArrayList. Binary Tree. LinkedList. HashTable?
+	//String value could one day be an object of any type
+	
+	private final String value;
+	private final ConfidenceTree cTree;
+	
+	private ConfidenceNode right;
+	private ConfidenceNode left;
+	private ConfidenceNode parent;
+
+	private ConfidencePairing confidenceRight;
+	private ConfidencePairing confidenceLeft;
+	private ConfidencePairing confidenceParent;
+
+	public ConfidenceNode(String value) {
+		cTree = new cTree();
+		cTree.root = this;
+	}
+
+}
