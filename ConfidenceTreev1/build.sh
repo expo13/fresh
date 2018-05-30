7194d8167fe1b6a53940aa5d3f1349d4ec8c5632
Craig Colquitt <colquitt.craig@gmail.com>
2018-05-30 07:03:31 -0600

    organization

--- ConfidenceTreev1/build.sh
+++ ConfidenceTreev1/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+#make necessary dirs
+mkdir -p bin/
+mkdir -p build/
+
 #Check length of arguments
 if [ $# -eq 0 ]
   then
 	echo "No arguments supplied - Compiling All"
 	javac -cp bin/ -d bin/ *.java
-
+	jar cfm build/ConfidenceTree.jar META-INF/MANIFEST.MF bin/com/
 else
 	echo "Compiling - $1"
 	javac -cp bin/ -d bin/ $1
