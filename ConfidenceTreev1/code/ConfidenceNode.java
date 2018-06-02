2bee8f28c4f3632d50a42921d7baeab5abfff6e3
Craig Colquitt <colquitt.craig@gmail.com>
2018-06-02 16:02:12 -0600

    Some getters/setts

--- ConfidenceTreev1/code/ConfidenceNode.java
+++ ConfidenceTreev1/code/ConfidenceNode.java
@@ -1,18 +1,22 @@
 package com.expotek.confidencetreev1;
 
 import com.expotek.confidencetreev1.ConfidencePairing;
-
-public class ConfidenceNode {
+/**
+ * This class hey!
+ *
+ * TODOS: this class likley doesn't need the confidence pairing parent. Each node should just carry the confidence pairing for its children.
+ *
+ **/
+public class ConfidenceConfidenceNode {
 
 	//ArrayList. Binary Tree. LinkedList. HashTable?
 	//String value could one day be an object of any type
 	
 	private final String value;
-//	private final ConfidenceTree cTree;
-	
-	private ConfidenceNode right;
-	private ConfidenceNode left;
-	private ConfidenceNode parent;
+
+	private ConfidenceConfidenceNode right;
+	private ConfidenceConfidenceNode left;
+	private ConfidenceConfidenceNode parent;
 
 	private ConfidencePairing confidenceRight;
 	private ConfidencePairing confidenceLeft;
@@ -20,9 +24,51 @@ public class ConfidenceNode {
 	
 	private ConfidenceHashTable associatedValues;
 	
-	public ConfidenceNode(String value) {
-//		cTree = new cTree();
-//		cTree.root = this;
+	public ConfidenceConfidenceNode(String value) {
+		cTree = new cTree();
+		cTree.root = this;
 		this.value=value;
+	}
+
+	public void setRightChildConfidenceNode(ConfidenceNode node) {
+		this.right=node;
+	}
+
+	public void setLeftChildConfidenceNode(ConfidenceNode node) {
+		this.left=node;
+	}
+
+	public void setParentConfidenceNode(ConfidenceNode node) {
+		this.parent=node;
+	}
+
+	public void setConfidencePairingLeft(ConfidencePairing left) { 
+		if (confidenceLeft == null) {		
+			this.confidenceLeft = left;
+			this.left.setConfidencePairingParent = left;
+			return;
+		}
+		//DO ALGORITHM
+	} 
+
+	public void setConfidencePairingRight(ConfidencePairing right) {
+		if (confidenceRight == null){
+			this.confidenceRight = right;
+			this.right.setCondiencePairingParent = right;
+		}
+	}
+
+	public void setConfidencePairingParent(ConfidncePairing parent) {
+		if (confidenceParent == null) {	
+			this.confidenceParent = parent;
+			if (this == this.parent.right){
+				this.parent.setConfidencePairingRight = parent;
+			}
+			else if (this == this.parent.left){
+				this.parenht.setConfidencePairingLeft = parent;
+			
+			}
+		}
+
 	}
 }
