93551fab80207118c8e7a509139df8c28feffb9e
Craig Colquitt <colquitt.craig@gmail.com>
2018-06-12 23:59:11 -0600

    search beginnings

---
+++ ConfidenceTreev1/code/FindInTree.java
@@ -0,0 +1,33 @@
+package com.expotek.confidencetreev1;
+
+public class FindInTree {
+
+	//depth first search combined with horizontal search 
+	public String findNode(String value, ConfidenceNode node){
+		
+		if (node == null || value == null){
+			//da fuck
+		}
+
+		if (node.right == null) {//we've reached a leaf somewhere
+			node = node.parent;
+			return findNode(value, node.left);
+		}
+		if (node.left == null) {
+		
+		}
+
+		if (searchNodeMap(node) == null) {
+			if (node.right !=null) {
+				return findNode(value,node.right);
+			}
+		}
+
+
+	}
+
+	public String searchNodeMap(ConfidenceNode node){
+	
+	}
+
+}
--- ConfidenceTreev1/code/Prune.java
+++ ConfidenceTreev1/code/Prune.java
@@ -2,4 +2,12 @@ package com.expotek.confidencetreev1;
 
 public class Prune {
 
+
+	public boolean binaryPrune(ConfidenceNode root) {
+	//horizontally traverse the binary tree .. moving the higher confidence and
+	//count value to positions of higher significance // rapid 
+	
+		ConfidenceQueueNode theStart = new ConfidenceQueueNode(root);
+
+	}
 }
