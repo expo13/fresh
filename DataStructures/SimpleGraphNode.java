036d0ce075f9f8daf53fa14ecf72687e64268818
Craig Colquitt <colquitt.craig@gmail.com>
2018-06-14 17:13:46 -0600

    added list to graph node

--- DataStructures/SimpleGraphNode.java
+++ DataStructures/SimpleGraphNode.java
@@ -3,5 +3,8 @@ package com.expotek.datastructures;
 public class SimpleGraphNode extends Node {
 	
 	//use Collection? use List? use Iterable?	
+	//default is list but can use a variety of ds's to reprsent local nodes
+	private List<SimpleGraphNode> simpleGraphNode;
+
 
 }
